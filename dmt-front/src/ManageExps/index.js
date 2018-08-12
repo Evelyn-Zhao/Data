b48365c4729181c356b200ad58465eedb3ee0219
Evelyn Zhao <u5631546@uds.anu.edu.au>
2018-08-13 02:07:27 +1000

    add side nav bar in exp manage page

--- dmt-front/src/ManageExps/index.js
+++ dmt-front/src/ManageExps/index.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
 import { withRouter } from "react-router-dom";
+import './ManageExps.css'
 
 class ManageExps extends Component {
     render(){
         return(
-            <p> hello </p>
+            <div className="sidenav">
+                <a href="#about">Add New Experiment</a>
+                <a href="#services">Manage Experiments</a>
+                <a href="#clients">Claim Open Experiment</a>
+                <a href="#contact">Help</a>
+            </div>
         );
     }
 }export default withRouter(ManageExps);
